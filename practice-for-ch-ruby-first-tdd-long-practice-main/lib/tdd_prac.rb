d1243d32a8064cf77517b9f799846c1948fd7c9b
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-04-21 15:38:43 -0700

    completed two_sum and my_transpose

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd_prac.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd_prac.rb
@@ -1,3 +1,19 @@
 def my_uniq(arr)
     arr.uniq
 end
+
+def two_sum(arr)
+    pairs = []
+    (0...arr.size).each do |i|
+        (0...arr.size).each do |j|
+            if j > i
+                pairs << [i, j] if arr[i] + arr[j] == 0
+            end
+        end
+    end
+    pairs
+end
+
+def my_transpose(arr)
+    arr.transpose
+end
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_prac_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_prac_spec.rb
@@ -3,7 +3,24 @@ require "tdd_prac"
 describe "my_uniq" do
     it "tests that an array's objects are unique" do
         arr = [1, 2, 1, 3, 3]
-        expect(my_uniq(arr)).to eq([1, 2, 3]) 
+        expect(my_uniq(arr)).to eq([1, 2, 3])
     end
 end
 
+describe "two_sum" do
+    it "returns the indices of the pairs that sum to zero" do
+        arr = [-1, 0, 2, -2, 1]
+        expect(two_sum(arr)).to eq([[0, 4], [2, 3]])
+    end
+    it "returns the pairs of indices in ascending order" do
+        arr = [-1, 0, 2, -2, 1]
+        expect(two_sum(arr)).to eq([[0, 4], [2, 3]])
+    end
+end
+
+describe "my_transpose" do
+    it "returns a 2D array with the rows and columns inverted" do
+        arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        expect(my_transpose(arr)).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+    end
+end
